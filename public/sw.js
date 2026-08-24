0c3c08ac2872ba5e463c172aa9cf0d0d038d7393
jhyxng9088 <j.hyxng9088@gmail.com>
2026-08-25 07:50:16 +0900

    Refresh PWA cache for Stage 2

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'school-shell-v3'
+const CACHE_NAME = 'school-shell-v4'
 const APP_SHELL = ['./', './manifest.webmanifest', './icon.svg']
 
 self.addEventListener('install', (event) => {
