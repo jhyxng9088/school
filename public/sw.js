cf8bff1b678c94190977a415fe1c8f13c63f4cc0
jhyxng9088 <j.hyxng9088@gmail.com>
2026-08-25 08:04:48 +0900

    Refresh timetable fixes in installed PWA

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'school-shell-v4'
+const CACHE_NAME = 'school-shell-v5'
 const APP_SHELL = ['./', './manifest.webmanifest', './icon.svg']
 
 self.addEventListener('install', (event) => {
