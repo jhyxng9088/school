813ad57e365d0f90db1741a9170253444e72f4e6
jhyxng9088 <j.hyxng9088@gmail.com>
2026-08-25 09:07:52 +0900

    Bump PWA cache for sheet refinements

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'school-shell-v7'
+const CACHE_NAME = 'school-shell-v8'
 const APP_SHELL = ['./', './manifest.webmanifest', './icon.svg', './school-sheet.css', './school-sheet.js']
 
 self.addEventListener('install', (event) => {
