bafe93637b513005561fca660746523dc7bb250d
jhyxng9088 <j.hyxng9088@gmail.com>
2026-08-25 13:37:50 +0900

    Use time-aware Home meal preview

--- src/stage3.js
+++ src/stage3.js
@@ -2,8 +2,8 @@ export {
   SUJI_SCHOOL,
   AcademicPage,
   AcademicPreview,
-  MealPreview,
   useSchoolData,
 } from './stage3-core.js'
 
+export { default as MealPreview } from './home-meal-preview.jsx'
 export { default as MealPage } from './meal-page.jsx'
