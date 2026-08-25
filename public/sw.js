915855f054377cf5a3e272b7f3d1dcf16c3b9c97
jhyxng9088 <j.hyxng9088@gmail.com>
2026-08-25 11:46:19 +0900

    Cache shared page motion and academic supplement

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'school-shell-v20'
-const APP_SHELL = ['./', './manifest.webmanifest', './icon.svg', './school-sheet.css', './school-refinements.css', './stage3-polish.css', './school-sheet.js', './school-timetable-motion.js']
+const CACHE_NAME = 'school-shell-v21'
+const APP_SHELL = ['./', './manifest.webmanifest', './icon.svg', './school-sheet.css', './school-refinements.css', './stage3-polish.css', './school-page-motion.css', './school-academic-supplement.js', './school-sheet.js', './school-timetable-motion.js']
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
