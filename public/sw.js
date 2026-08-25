30f1dca7671bc26cda06018112d07723a8b0cbd5
jhyxng9088 <j.hyxng9088@gmail.com>
2026-08-25 11:58:06 +0900

    Refresh cache for post-render meal motion

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'school-shell-v22'
+const CACHE_NAME = 'school-shell-v23'
 const APP_SHELL = ['./', './manifest.webmanifest', './icon.svg', './school-sheet.css', './school-refinements.css', './stage3-polish.css', './school-page-motion.css', './school-academic-supplement.js', './school-sheet.js', './school-timetable-motion.js', './school-meal-motion.js']
 
 self.addEventListener('install', (event) => {
