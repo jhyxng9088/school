393898154647f274b2c8db6adec2ac0d48e3f90a
jhyxng9088 <j.hyxng9088@gmail.com>
2026-08-26 18:27:15 +0900

    Deploy native text centering fix v107

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'school-shell-v106'
+const CACHE_NAME = 'school-shell-v107'
 const APP_SHELL = ['./', './manifest.webmanifest', './icon.svg', './school-refinements.css', './stage3-polish.css', './school-page-motion.css', './reminder-list-motion.css', './school-home-live.css', './first-run-notice.css', './school-timetable-motion.js', './school-home-nav.js', './first-run-notice.js']
 
 self.addEventListener('install', (event) => {
