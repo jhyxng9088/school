eb923ddc15d6ec9dd8d0f1ebe9c7782edecccaf4
jhyxng9088 <j.hyxng9088@gmail.com>
2026-08-26 22:05:20 +0900

    Update S-Hub home screen icon v116

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'school-shell-v115'
-const APP_SHELL = ['./', './manifest.webmanifest', './icon.svg', './school-refinements.css', './stage3-polish.css', './school-page-motion.css', './reminder-list-motion.css', './school-home-live.css', './first-run-notice.css', './samsung-apple-nav-icons.css', './school-timetable-motion.js', './school-home-nav.js', './first-run-notice.js']
+const CACHE_NAME = 'school-shell-v116'
+const APP_SHELL = ['./', './manifest.webmanifest', './icon-512.png', './icon-180.png', './school-refinements.css', './stage3-polish.css', './school-page-motion.css', './reminder-list-motion.css', './school-home-live.css', './first-run-notice.css', './samsung-apple-nav-icons.css', './school-timetable-motion.js', './school-home-nav.js', './first-run-notice.js']
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
