389e3f8d53130229d7d8423a4d72eb883b2d0f6d
jhyxng9088 <j.hyxng9088@gmail.com>
2026-08-27 10:53:03 +0900

    Deploy unread indicators and timetable fix

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'school-shell-v128'
+const CACHE_NAME = 'school-shell-v129'
 const APP_SHELL = ['./', './manifest.webmanifest', './icon.svg', './icon-android.svg', './school-refinements.css', './stage3-polish.css', './school-page-motion.css', './reminder-list-motion.css', './school-home-live.css', './first-run-notice.css', './samsung-apple-nav-icons.css', './samsung-nav-icon-fixes.css', './samsung-nav-meal.svg', './samsung-nav-academic.svg', './school-timetable-motion.js', './school-home-nav.js', './first-run-notice.js', './notification-routing.js']
 
 self.addEventListener('install', (event) => {
