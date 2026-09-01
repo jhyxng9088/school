341aa77de4a89f5697feab35724ceff633cf5785
jhyxng9088 <j.hyxng9088@gmail.com>
2026-09-01 23:02:05 +0900

    fix(v2): keep feature-tour cache regression marker

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'school-shell-v157'
+const CACHE_NAME = 'school-shell-v155-v2-update1'
 const NOTIFICATION_PROFILE_CACHE = 'school-notification-profile-v1'
 const NOTIFICATION_PROFILE_URL = new URL('./__notification-tone-profile__', self.registration.scope).href
 const PERSONALIZED_STUDENT_KEY = 'student-a63dc064d4c5227e'
