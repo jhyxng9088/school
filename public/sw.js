964ece90a0443f12216fc0ff3d830baf1ac47c14
jhyxng9088 <j.hyxng9088@gmail.com>
2026-09-01 23:25:33 +0900

    chore(pwa): refresh shell after Samsung parity sync

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'school-shell-v155-v2-update3'
+const CACHE_NAME = 'school-shell-v155-v2-update4'
 const NOTIFICATION_PROFILE_CACHE = 'school-notification-profile-v1'
 const NOTIFICATION_PROFILE_URL = new URL('./__notification-tone-profile__', self.registration.scope).href
 const PERSONALIZED_STUDENT_KEY = 'student-a63dc064d4c5227e'
