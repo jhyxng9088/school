a53f5c0ccb9389f270fd810073cc323481080cc1
jhyxng9088 <j.hyxng9088@gmail.com>
2026-09-10 07:25:51 +0900

    fix: load base styles during school onboarding

--- src/app-bootstrap.jsx
+++ src/app-bootstrap.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
+import './styles.css'
 import { prepareClientDataGeneration, readStudentProfile, saveStudentProfile } from './school-sync.js'
 import { StudentSetup } from './student-setup.jsx'
 
