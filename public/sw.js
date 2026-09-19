9d5817ac9b74bc4835cc932b3e71dc2d162d771e
jhyxng9088 <j.hyxng9088@gmail.com>
2026-09-20 01:38:06 +0900

    chore: refresh shell for launch and motion fixes

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'school-shell-v161-launch-refresh'
+const CACHE_NAME = 'school-shell-v162-launch-motion'
 const NOTIFICATION_PROFILE_CACHE = 'school-notification-profile-v1'
 const NOTIFICATION_PROFILE_URL = new URL('./__notification-tone-profile__', self.registration.scope).href
 const PERSONALIZED_STUDENT_KEY = 'student-a63dc064d4c5227e'
